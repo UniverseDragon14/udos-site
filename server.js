cc5d253f530952a4517a70dad6c1ce2b5de13712
Universal Dragon <221464346+UniverseDragon14@users.noreply.github.com>
2026-05-28 20:17:54 +0400

    Add protected NOVA Dev Doctor V2 endpoints

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import doctorRoutes from "./doctor_routes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -17,6 +18,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(__dirname));
+app.use("/api/doctor", doctorRoutes);
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
